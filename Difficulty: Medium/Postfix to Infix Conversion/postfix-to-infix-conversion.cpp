9d1ee9b76839eb1129a9a09d3007f36ffc31b7a7
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-09-19 12:11:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Infix Conversion/postfix-to-infix-conversion.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string postToInfix(string s) {
+        // Write your code here
+        stack<string> st;
+        int i=0;
+        while(i<s.size()){
+            // directly push an operand into the stack
+            if((s[i]>='A' && s[i]<='Z')||
+               (s[i]>='a' && s[i]<='z')||
+               (s[i]>='0' && s[i]<='9'))
+               {
+                   st.push(string(1,s[i]));
+               }
+            // when an operator is encountered , take the last two operands from the stack and put that operator b/w them
+            // and then wrap it in braces
+            else{
+                string s1 = st.top();
+                st.pop();
+                string s2 = st.top();
+                st.pop();
+                string converted = '('+ s2 + s[i] + s1 + ')';
+                st.push(converted);
+            }
+            i++;
+        }
+        return st.top();
+    }
+};
