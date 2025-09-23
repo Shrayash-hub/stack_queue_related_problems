cac2e23d5c065f417cbc8b055963b05f3f45ef0d
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-09-23 10:35:05 +0530

    Time: 11 ms (95.11%) | Memory: 81.3 MB (82.13%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        // we can increase the width until we encounter a smaller bar , that means we have look for next smaller and previous smaller element
+        // we will be maintaining a monotonix increasing stack
+        stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+        // calculating nse and pse is going to lot of space and time,so we will be calculating it in a single traversal , by doing some manipulations
+        for(int i=0;i<n;i++){
+            // as we encounter a smaller bar , its going to become the nse for top element , then we will pop that element and now new top will become the previous smaller element for that popped element , that how we get to know about both nse and pse of a element
+            while(!st.empty() && heights[i]<heights[st.top()]){
+                int element = st.top();
+                st.pop();
+                int nse = i;
+                int pse = st.empty()? -1:st.top();
+                maxArea = max(maxArea,heights[element] *(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int nse = n;
+            int element = st.top();
+            st.pop();
+            int pse = st.empty() ? -1:st.top();
+            maxArea = max(maxArea,heights[element] *(nse-pse-1));
+        }
+        return maxArea;
+    }
+};
