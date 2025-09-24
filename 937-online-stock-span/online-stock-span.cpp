bbae8ce2e79d5be06adc1b7c08e8154ec3413dde
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-09-24 11:15:04 +0530

    Time: 22 ms (88.21%) | Memory: 92 MB (58.97%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -4,14 +4,15 @@ public:
     int index;
     StockSpanner() {
         index = -1;
-        //st.clear();
     }
     
     int next(int price) {
         index = index+1;
+        // we have to find the index of previous greater element 
         while(!st.empty() && st.top().first<=price){
             st.pop();
         }
+        // ans will be the diffrence between current index and index of previous greater element
         int ans = index - (st.empty()? -1:st.top().second);
         st.push({price,index});
         return ans;
